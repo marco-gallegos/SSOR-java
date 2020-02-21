ff24232f322c1bcc4356a02b1d7c02fb274665fd
marco gallegos <ma_galeza@hotmail.com>
2020-02-20 21:16:43 -0600

    [imp] practica 7: finalizada
    se termina con los requisitos de la practica 7 con todo y los puntos
    extra

--- src/main/java/Arreglo.java
+++ src/main/java/Arreglo.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Random;
 
 public class Arreglo {
@@ -11,7 +12,7 @@ public class Arreglo {
 
         char[] letras = "BCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();// = new char[]('BCDEFGHIJKLMNOPQRSTUVWXYZ');
         char[] arreglo = new char[tam];
-        char[] modificado = new char[tam];
+        char[] original = new char[tam];
 
         int tamletras = letras.length;
 
@@ -20,33 +21,59 @@ public class Arreglo {
         System.out.println(String.format("size tam : %s", tam));
 
         for (int i = 0; i < tam; i++){
-            //System.out.println(i);
             arreglo[i] = letras[r.nextInt(tamletras)];
+        }
+        a.printarray(arreglo);
+        original = Arrays.copyOf(arreglo, tam);
+
+        a.metodo(arreglo);//get bytes
+
+        a.printarray(arreglo);
+
+        a.printdiff(arreglo, original);
+    }
+
+    private void printarray(char[] arreglo){
+        int tam = arreglo.length;
+        for (int i = 0; i < tam; i++){
             System.out.print(arreglo[i]);
         }
         System.out.println();
-
-        a.metodo(letras);//get bytes
+    }
+    private void printdiff(char[] arreglo, char[] original){
+        System.out.println("imprimo solo lo que cambio");
+        int tam = arreglo.length;
+        for (int i = 0; i < tam; i++){
+            if (arreglo[i] != original[i]){
+                System.out.print(arreglo[i]);
+            }else{
+                System.out.print("*");
+            }
+        }
+        System.out.println();
     }
 
     private void metodo(char[] arreglo) {
-        String x = "BATMAN";
+        String x = "GAVER";
         int tamarreglo = arreglo.length;
-        int vecesacopiar = tamarreglo/x.length();
+        int tamx = x.length();
+        int vecesacopiar = tamarreglo/tamx;
+        int vecescopiado = 0;
+        int contadorcopia = 0;
 
+        for (int i = 0; i < tamarreglo; i++){
+            if(contadorcopia>=tamx){
+                contadorcopia=0;
+                vecescopiado+=1;
+            }
+            if(vecescopiado >= vecesacopiar){
+                break;
+            }
+            arreglo[i] = x.charAt(contadorcopia);
+            contadorcopia+=1;
+        }
+        System.out.println(String.format("veces copiado %s", vecescopiado));
     }
 
     // es un extra si solo muestras los modificados
 }
-/*
-    1. Crear un arreglo de caracteres de forma aleatoria con puras letras Mayúsculas.
-    2. Crear un método que reciba un parámetro arreglo de caracteres.
-    3. el método copie en dicho arreglo el contenido de una cadena X
-    4. Esta cadena X contenga puras Mayúsculas y sea menor al arreglo de caracteres.
-    5. copiar en el arreglo la cadena, tantas veces como quepa completa
-    6. no usar funciones de copiado del API de Java
-    7. el método reciba al menos el parámetro arreglo
-    8. justo después de salir de la subrutina, se imprima el contenido del arreglo de caracteres
-    9. usar solo variables locales y parámetros
-    10. imprimir primero el arreglo original, luego el arreglo corregído y por último sólo las celdas modificadas.
- */
